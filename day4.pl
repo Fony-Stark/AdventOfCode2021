21bce8ada32b7d2dba8d58b98ded3a0a2566d434
Fony-Stark <kholt19@student.aau.dk>
2021-12-12 21:35:35 +0100

    Weird progress...

--- day4.pl
+++ day4.pl
@@ -14,8 +14,27 @@ stream_line(In, Line) :-
 
 charles_part1(Score):-
 	findall(Line, file_line("Day4Input.txt", Line), Lines),
-	
-	Lines = [Draw | Raw_boards],
-	board_process(Raw_boards, Boards),
 
-	winning_board(Boards, Draw, Score).
+	Lines = [Draw | Raw_input],
+    Raw_input = [EmptyLine | Raw_boards],
+	board_process(Raw_boards, [], Boards),
+    Boards = [B | S],
+    write(B).
+	%winning_board(Boards, Draw, Score).
+
+board_process([], CBoard, [CBoard]).
+board_process([Raw | Raws], CBoard, [CBoard | Boards]):-
+    Raw = "",
+    board_process(Raws, [], Boards).
+board_process([Raw | Raws], CBoard, Boards):-
+    process_line(Raw, CB),
+    append(CBoard, CB, CBBoard),
+    board_process(Raws, CBBoard, Boards).
+
+process_line(String, List):-
+    split_string(String, " ", "", NString),
+    delete(NString, "", LongList),
+    length(LongList, LongLength),
+    Raw_length is sqrt(LongLength),
+    round(Raw_length, Length),
+    space_out(LongList, 0, Length, List).
